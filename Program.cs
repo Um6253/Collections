7a5e75c92c242280f87a4509502165025c67a4c2
Um6253 <um6253@srmist.edu.in>
2024-02-04 22:12:28 +0530

    [Mani]:Linear Search Using  Queue in C#

--- Program.cs
+++ Program.cs
@@ -12,59 +12,62 @@ namespace Collections
 
         static void Main(string[] args)
         {
+
+            Queue<int> numbers = new Queue<int>();
+
+            Console.WriteLine("Enter numbers (enter -1 to stop):");
+
             
-                Stack<int> numbers = new Stack<int>();
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
 
-                Console.WriteLine("Enter numbers (enter -1 to stop):");
+                if (input == "-1")
+                    break;
 
-                
-                while (true)
+                if (int.TryParse(input, out int number))
                 {
-                    Console.Write("Enter a number: ");
-                    string input = Console.ReadLine();
-
-                    if (input == "-1")
-                        break;
-
-                    if (int.TryParse(input, out int number))
-                    {
-                        numbers.Push(number);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input. Please enter a valid integer.");
-                    }
-                }
-
-                Console.WriteLine("\nEnter the number to search:");
-                int searchNumber;
-                while (!int.TryParse(Console.ReadLine(), out searchNumber))
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid integer to search:");
-                }
-
-                
-                bool found = false;
-                foreach (int num in numbers)
-                {
-                    if (num == searchNumber)
-                    {
-                        found = true;
-                        break;
-                    }
-                }
-
-               
-                if (found)
-                {
-                    Console.WriteLine($"The number {searchNumber} is found in the stack.");
+                    numbers.Enqueue(number);
                 }
                 else
                 {
-                    Console.WriteLine($"The number {searchNumber} is not found in the stack.");
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
                 }
-
             }
+
+            Console.WriteLine("\nEnter the number to search:");
+            int searchNumber;
+            while (!int.TryParse(Console.ReadLine(), out searchNumber))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer to search:");
+            }
+
+            
+            bool found = false;
+            int count = numbers.Count;
+            for (int i = 0; i < count; i++)
+            {
+                int num = numbers.Dequeue();
+                if (num == searchNumber)
+                {
+                    found = true;
+                    break;
+                }
+                numbers.Enqueue(num);
+            }
+
+            
+            if (found)
+            {
+                Console.WriteLine($"The number {searchNumber} is found in the queue.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {searchNumber} is not found in the queue.");
+            }
+
+        }
     }
         
     
