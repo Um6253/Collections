b2593424e500179053f7d4e0deb724adea00777a
Um6253 <um6253@srmist.edu.in>
2024-02-04 21:36:03 +0530

    [Mani]:Linear Search Using Hashset in C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,66 @@ namespace Collections
 {
     internal class Program
     {
+        static bool LinearSearchWithHashSet(int[] array, int target)
+        {
+            HashSet<int> set = new HashSet<int>(array);
+
+            return set.Contains(target);
+        }
+
         static void Main(string[] args)
         {
+            HashSet<int> numbers = new HashSet<int>();
+
+            Console.WriteLine("Enter numbers (enter -1 to stop):");
+
+           
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
+
+                if (input == "-1")
+                    break;
+
+                if (int.TryParse(input, out int number))
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            }
+
+            Console.WriteLine("\nEnter the number to search:");
+            int searchNumber;
+            while (!int.TryParse(Console.ReadLine(), out searchNumber))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer to search:");
+            }
+
+            
+            bool found = false;
+            foreach (int num in numbers)
+            {
+                if (num == searchNumber)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            
+            if (found)
+            {
+                Console.WriteLine($"The number {searchNumber} is found in the set.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {searchNumber} is not found in the set.");
+            }
+            Console.ReadLine();
         }
     }
 }
